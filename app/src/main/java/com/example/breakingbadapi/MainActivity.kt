dea8927ed6f8ac737b614685245fe6c6ed1dbb11
Armando Santos <armandosantosec@gmail.com>
2021-06-29 13:17:14 -0600

    add the method removeFavorites

--- app/src/main/java/com/example/breakingbadapi/MainActivity.kt
+++ app/src/main/java/com/example/breakingbadapi/MainActivity.kt
@@ -19,12 +19,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerAdapter: BreakingBadAdapter
 
+
     private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getFavorites()
+        Log.d(TAG, "recyclerInit: ${getFavorites()}")
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.breakingbadapi.com/api/")
@@ -50,27 +52,27 @@ class MainActivity : AppCompatActivity() {
                     //Analizamos
                     val favorites = getFavorites()?.split(",")
 
+                    Log.d(TAG, "onResponse: ${favorites!!.size}")
                     // Lista definitiva
-                    var temp: List<Character?>
-                    var temp2: List<Character>
                     var counter = 0
-
-                    for (i in characters!!.indices) {//analizo el array que viene del API
-                        for (j in favorites!!.indices) {//Analizo solo los favoritos
-                            if (characters[i].char_id == favorites[j].toInt()) {
-                                //los fav que hagan match
-                                characters[i].isSelected = true
-                                //temp?.toMutableList()?.add(counter, characters[i])
-                                counter++
-                            } else {
-                                //los que no esten en fav
-                                //temp2?.toMutableList()?.add(counter, characters[i])
-                                counter++
+                    if (favorites.isNotEmpty() && favorites[0] != "") {
+                        for (i in characters!!.indices) {//analizo el array que viene del API
+                            for (j in favorites!!.indices) {//Analizo solo los favoritos
+                                if (characters[i].char_id == favorites[j].toInt()) {
+                                    //los fav que hagan match
+                                    characters[i].isSelected = true
+                                    //temp?.toMutableList()?.add(counter, characters[i])
+                                    counter++
+                                } else {
+                                    //los que no esten en fav
+                                    //temp2?.toMutableList()?.add(counter, characters[i])
+                                    counter++
+                                }
                             }
                         }
-                    }
 
-                    Log.d(TAG, "onResponse: ")
+                        Log.d(TAG, "onResponse: ")
+                    }
 
                     //val valor = (temp?.size?.minus(1))
                     //for (i in valor!! until characters.size) {
@@ -94,36 +96,32 @@ class MainActivity : AppCompatActivity() {
         recyclerAdapter = BreakingBadAdapter(this) { item, position, status ->
 
             //save to favorites
-            if (status)
+            if (status) {
                 SharedPrefManager().setStringPrefVal(
                     applicationContext,
                     getString(R.string.kfav),
                     addToFavorites(item.char_id)
                 )
-            else {
+                Log.d(TAG, "recyclerInit: ${getFavorites()}")
+
+            } else {
                 //Eliminar
+                Log.d(TAG, "recyclerInit: ELIMINANDO ACCION")
                 val temp = getFavorites()
-                val list = temp?.split(",")
-                var nueva = ""
-                for (i in list!!.indices) {
-                    if (!list[i].equals(item.char_id)) {
-                        if (nueva.isEmpty())
-                            nueva = list[i]
-                        else {
-                            nueva = "$nueva,${list[i]}"
-                        }
-                    }
-                }
+                val repl = item.char_id
+                var list = temp!!.replace("$repl", "")
+                var esComa = false
+                var current = ""
 
-                Log.d(TAG, "recyclerInit: $nueva")
+                val charItems = list.toCharArray()
 
+                val favorites = removeFavorites(charItems)
+               // Log.d(TAG, "Almacenar: $favorites")
                 SharedPrefManager().setStringPrefVal(
                     applicationContext,
                     getString(R.string.kfav),
-                    nueva
+                    favorites
                 )
-
-                getFavorites()
             }
 
             it[position].isSelected = status
@@ -134,19 +132,59 @@ class MainActivity : AppCompatActivity() {
         recyclerAdapter.setSuperHeroes(it)
     }
 
+    private fun removeFavorites(charItems: CharArray): String {
+        var favorites = ""
+        for (i in charItems.indices) {
+            //Log.d(TAG, "char -> pos[$i]: ${charItems[i]}")
+            if (charItems[0] == ',') {
+                //No hacer nada
+                charItems[i] = ' '
+                //Log.d(TAG, "removeFavorites: Primera es una coma")
+            } else {
+                //validamos si hay comas al ingresar aqui
+                val item = charItems[i]
+                val isComa = item == ','
+                if (isComa) {
+                    //Validamos la posicion anterior
+                    val itemBefore = charItems[i - 1]
+                    val isComa2 = itemBefore == ','
+                    val isComa3 = itemBefore == ' '
+                    if (!isComa2 && !isComa3)
+                        favorites += charItems[i]
+                } else
+                    favorites += charItems[i]
+            }
+        }
+
+       // Log.d(TAG, "removeFavorites0: $favorites")
+        val longitud = favorites.length
+        if (longitud > 1) {
+            val finalComma = favorites.substring(longitud.minus(1), longitud)
+            val isComaa = finalComma.equals(",")
+            if (isComaa) {
+                val temp = favorites
+                favorites = temp.substring(0, longitud - 1)
+            }
+        }
+
+        return favorites
+    }
+
     private fun getFavorites() =
         SharedPrefManager().getStringVal(applicationContext, getString(R.string.kfav))
 
     private fun addToFavorites(charId: Int?): String? {
         val temp = SharedPrefManager().getStringVal(applicationContext, getString(R.string.kfav))
-        var register = ""
+        var register: String
         if (temp!!.isEmpty()) {
             register = charId.toString()
         } else {
             register = "$temp,$charId"
         }
+
         return register
     }
 
 
 }
+
